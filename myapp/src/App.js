af0fd049fa239ec4a029a5d7606c61761ef2fa26
samer-hoblos <sanerhoblos94@gmail.com>
2021-03-25 16:50:55 +0200

    Weather API: HTML/CSS

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,21 +3,33 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="WeatherAPI">
+      <header className="SearchContainer">
+        <input type="text" value="Type in a city name"></input>
+        <button type="submit" >FIND WEATHER</button>
       </header>
+      <main className="MainApp">
+        <div className="pic1">
+          <img src="https://placekitten.com/150/150" alt="kitten"></img><p>Hello samer</p><br></br>
+          <p>Hello Hasan</p>
+        </div>
+
+        <div className="temp24" >
+          <div><p>3</p><img src="https://placekitten.com/50/50" alt="kitten"></img><p>8</p></div>
+
+          <div><p>6</p><img src="https://placekitten.com/50/50" alt="kitten"></img><p>9</p></div>
+
+          <div><p>9</p><img src="https://placekitten.com/50/50" alt="kitten"></img><p>14</p></div>
+
+          <div><p>12</p><img src="https://placekitten.com/50/50" alt="kitten"></img><p>17</p></div>
+
+          <div><p>15</p><img src="https://placekitten.com/50/50" alt="kitten"></img><p>18</p></div>
+
+          <div><p>18</p><img src="https://placekitten.com/50/50" alt="kitten"></img><p>16</p></div>
+
+          <div><p>21</p><img src="https://placekitten.com/50/50" alt="kitten"></img><p>13</p></div>
+        </div>
+      </main>
     </div>
   );
 }
